73946155e533f69a9ac0cc3f2a5bbcd981a6dbc4
曹弘扬 <296786475@qq.com>
2019-07-10 16:40:50 +0800

    增加componment

---
+++ Adam_20190709_U17/Component/ParallaxHeader/UIScrollView+ParallaxHeader.swift
@@ -0,0 +1,21 @@
+//
+//  UIScrollView+ParallaxHeader.swift
+//  Adam_20190709_U17
+//
+//  Created by Adonis_HongYang on 2019/7/10.
+//  Copyright © 2019 Adonis_HongYang. All rights reserved.
+//
+
+import UIKit
+import ObjectiveC.runtime
+
+/**
+ A UIScrollView extension with a ParallaxHeader.
+ */
+extension UIScrollView {
+    private struct AssociatedKeys {
+        static var descriptiveName = "AssociatedKeys.DescriptiveName.parallaxHeader"
+    }
+    
+    
+}
