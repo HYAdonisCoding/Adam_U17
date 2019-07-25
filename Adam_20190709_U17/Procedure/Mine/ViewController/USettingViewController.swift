02eb23699bb3df363411bc0651e1eccb6cec0c72
曹弘扬 <296786475@qq.com>
2019-07-25 17:01:18 +0800

    Finish Mine page

---
+++ Adam_20190709_U17/Procedure/Mine/ViewController/USettingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  USettingViewController.swift
+//  Adam_20190709_U17
+//
+//  Created by Adonis_HongYang on 2019/7/25.
+//  Copyright © 2019 Adonis_HongYang. All rights reserved.
+//
+
+import UIKit
+
+class USettingViewController: UBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func configNavigationBar() {
+        super.configNavigationBar()
+        navigationItem.title = "设置"
+    }
+}
